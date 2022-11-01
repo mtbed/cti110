37cd9a721f3802576b1f5ec0456b98bf63dd3302
mtbed <113650939+mtbed@users.noreply.github.com>
2022-11-01 13:58:12 -0400

    Create grades.py
    User can enter 6 grades

---
+++ grades.py
@@ -0,0 +1,14 @@
+# CTI-110
+# P4HW1 - Grade List
+# Matthew Bedsole
+# 11/1
+
+# Ask for 6 grads for the 6 modules
+# Add them to a list.
+
+grades = []
+
+for grade in range(6):
+        grade = int(input("Enter grade: "))
+        grades.append(grade)
+print(grades)
